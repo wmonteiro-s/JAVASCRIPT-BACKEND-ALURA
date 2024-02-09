f327bd3d70cc26ee0558a36d96119fb1c59edd38
Wesley Monteiro <wmonteiro873@gmail.com>
2024-02-09 15:43:41 -0300

    Conversões de tipo no JS

---
+++ tipo_number/conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos
+
+// conversao implícita
+const numero = 456
+const numeroString = '456a'
+
+console.log(numero + numeroString)
+
+// conversao explícita
+    /*
+    String()
+    Number()
+    */
+console.log(numero + Number(numeroString))
